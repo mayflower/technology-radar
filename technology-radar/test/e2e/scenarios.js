7393e78372f957f655433966b730d837ff5e142a
Christian Albrecht <christian.albrecht@mayflower.de>
2013-04-12 15:13:34 +0200

    Yai first failing test

--- technology-radar/test/e2e/scenarios.js
+++ technology-radar/test/e2e/scenarios.js
@@ -2,48 +2,41 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('my app', function () {
 
-  beforeEach(function() {
-    browser().navigateTo('/index.html');
-  });
-
-
-  it('should start with root', function() {
-    expect(browser().location().url()).toBe("/view1");
-  });
-
-  describe('technology resource list', function() {
-
-      it('should have a detail link', function() {
-          expect(element('#technologies a', 'link one').count()).toBeGreaterThan(0);
-      });
-
-      it('should redirect to detail view', function() {
-
-          element('#technologies a:first', 'detail link').click();
-          expect(browser().location().url()).toEqual('/detail/5167f9288e2d6d3214000001');
-
-      });
-      
-      it('should redirect to detail view', function() {
-
-          element('#technologies a:first', 'detail link').click();
-          expect(browser().location().url()).toEqual('/detail/5167f9288e2d6d3214000001');
-
-      })
-  });
-
-  /*describe('Technologies', function() {
-    it('should display 3 technologies', function() {
-      expect(repeater('#technologies li').count()).toBe(4);
- 
-      input('query').enter('nodejs');
-      expect(repeater('#technologies li').count()).toBe(1);
- 
-      input('query').enter('adopt');
-      expect(repeater('#technologies li').count()).toBe(2);
+    beforeEach(function () {
+        browser().navigateTo('/index.html');
     });
-  });*/
 
+
+    it('should start with root', function () {
+        expect(browser().location().url()).toBe("/view1");
+    });
+
+    describe('technology resource list', function () {
+
+        var detailPath = '/detail/5167f9288e2d6d3214000001';
+
+        it('should have a detail link', function () {
+            expect(element('#technologies a', 'link one').count()).toBeGreaterThan(0);
+        });
+
+        it('should redirect to detail view', function () {
+
+            element('#technologies a:first', 'detail link').click();
+            expect(browser().location().url()).toEqual(detailPath);
+
+        });
+
+        it('should redirect to detail view', function () {
+
+            element('#technologies a:first', 'detail link').click();
+            expect(browser().location().url()).toEqual(detailPath);
+
+        });
+
+        describe('Detail View should have an edit link', function() {
+            expect(element('#technologies a:first', 'edit link').count()).toBe(1);
+        });
+    });
 });
